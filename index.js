eb0715e422458354872171df5fe18450b13da757
Shirshak <shirshak1503@gmail.com>
2026-08-10 02:50:44 +0530

    added env functionality

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
 import userrouter from './routes/users.js';
-
+const port = process.env.PORT;
 app.use('/users', userrouter);
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
